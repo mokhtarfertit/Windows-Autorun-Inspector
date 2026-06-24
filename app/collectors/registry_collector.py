d59e4600f4f5f111a52be59b14f1336d52ad0252
mokhtar <mokhtarfertit@gmail.com>
2026-06-24 22:03:34 +0100

    start with implement collectors files

---
+++ app/collectors/registry_collector.py
@@ -0,0 +1,11 @@
+import winreg
+
+
+class RegistryCollector:
+    def __init__(self):
+        self.registry_paths = [
+            (winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run"),
+            (winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\RunOnce"),
+            (winreg.HKEY_LOCAL_MACHINE, r"Software\Microsoft\Windows\CurrentVersion\Run"),
+            (winreg.HKEY_LOCAL_MACHINE, r"Software\Microsoft\Windows\CurrentVersion\RunOnce"),
+        ]
