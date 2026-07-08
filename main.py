dd24073adce1b8065878e1011f7b3d1ceb52383c
mokhtar <mokhtarfertit@gmail.com>
2026-07-08 14:40:16 +0100

    we update main for to load from main of full porject

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-from app.collectors.registry_collector import RegistryCollector
-
-def main():
-    collector = RegistryCollector()
-    entries = collector.collect()
-
-    print(f"Found {len(entries)} registry autorun entries")
-
-    for entrey in entries:
-        print("-" * 60)
-        print(f"Name: {entrey['name']}")
-        print(f"Command: {entrey['command']}")
-        print(f"Registry path: {entrey['registry_path']}")
-        print(f"Timestamp: {entrey['timestamp']}")
+from app.main import main
 
 if __name__ == "__main__":
     main()
